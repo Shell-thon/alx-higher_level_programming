894a64fd4a25ff75ae0754456984706730729727
Mubarek Seid <mubareksd@gmail.com>
2022-08-23 12:19:08 +0300

    format

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for c in range(ord('a'), ord('z') + 1):
-    if c != ord('e') and c != ord('q'):
-        print("{:c}".format(c), end="")
+for ch in range(ord('a'), ord('z') + 1):
+    if ch != ord('e') and ch != ord('q'):
+        print(f"{ch:c}", end="")
