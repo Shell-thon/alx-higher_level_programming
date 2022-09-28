9fe70b2eb51ab60767eaee8139e215864eb0d180
Mubarek Seid <mubareksd@gmail.com>
2022-09-28 21:52:25 +0300

    task 14

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -20,7 +20,7 @@ if __name__ == "__main__":
             code = int(words[-2])
             if code in codes:
                 codes[code] += 1
-        except IndexError:
+        except ValueError:
             pass
 
     def print_stats():
