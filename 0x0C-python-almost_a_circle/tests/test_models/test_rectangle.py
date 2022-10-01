b195296107715aed9a5c461d8a5762e13b7f28ba
Mubarek Seid <mubareksd@gmail.com>
2022-10-01 22:57:56 +0300

    task 2

---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+test_rectangle module
+"""
+import unittest
+from models.base import Base
+from models.rectangle import Rectangle
+
+
+class TestRectangle(unittest.TestCase):
+    """
+    test class for Rectangle class
+    """
+    def setUp(self):
+        Base._Base__nb_objects = 0
+
+    def test_1(self):
+        """
+        test 1
+        """
+        r1 = Rectangle(10, 2)
+        self.assertEqual(r1.id, 1)
+
+        r2 = Rectangle(2, 10)
+        self.assertEqual(r2.id, 2)
+
+        r3 = Rectangle(10, 2, 0, 0, 12)
+        self.assertEqual(r3.id, 12)
+
+
+if __name__ == '__main__':
+    unittest.main()
