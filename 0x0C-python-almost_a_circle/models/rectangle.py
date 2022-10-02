41638ac75668f1b96b3de59b7a91a522a2cd0ca8
Mubarek Seid <mubareksd@gmail.com>
2022-10-02 11:12:25 +0300

    task 6

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -25,6 +25,12 @@ class Rectangle(Base):
         self.x = x
         self.y = y
 
+    def __str__(self):
+        """
+        overriden __str__ method
+        """
+        return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.__x, self.__y, self.__width, self.__height)
+
     @property
     def width(self):
         """ width getter """
