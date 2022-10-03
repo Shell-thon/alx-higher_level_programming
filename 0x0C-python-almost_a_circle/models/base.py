9bcbb9f864d5d8adec19b5294913a3885c40f3f2
Mubarek Seid <mubareksd@gmail.com>
2022-10-03 12:24:17 +0300

    task 15

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -3,6 +3,7 @@
 base module
 """
 import json
+import os
 
 
 class Base:
@@ -23,54 +24,13 @@ class Base:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
 
-    @staticmethod
     def to_json_string(list_dictionaries):
         """
-        returns the JSON string representation of list_dictionaries.
+        to_json_string - returns the JSON string representation of list_dictionaries.
+        Args:
+            list_dictionaries: is a list of dictionaries
         """
-        if list_dictionaries is None:
+        if list_dictionaries is None or list_dictionaries == []:
             return "[]"
         else:
             return json.dumps(list_dictionaries)
-
-    @classmethod
-    def save_to_file(cls, list_objs):
-        """
-        writes the JSON string representation of list_objs to a file.
-        Args:
-            cls: 
-            list_objs: is a list of instances who inherits of Base
-        """
-        filename = cls.__name__ + ".json"
-        text = []
-        if list_objs is not None:
-            for lst in list_objs:
-                text.append(lst.to_dictionary())
-        with open(filename, mode="w", encoding="utf-8") as f:
-            return f.write(Base.to_json_string(text))
-
-    @staticmethod
-    def from_json_string(json_string):
-        """
-        returns the list of the JSON string representation json_string.
-        Args:
-            json_string: is a string representing a list of dictionaries
-        """
-        if json_string is None or len(json_string) == 0:
-            return []
-        else:
-            return json.loads(json_string)
-
-    @classmethod
-    def create(cls, **dictionary):
-        """Returns an instance with all attributes already set.
-        Args:
-            - dictionary: used as **kwargs
-        Returns: instance created
-        """
-        if cls.__name__ == 'Rectangle':
-            dummy = cls(1, 1)
-        elif cls.__name__ == 'Square':
-            dummy = cls(1)
-        dummy.update(**dictionary)
-        return dummy
