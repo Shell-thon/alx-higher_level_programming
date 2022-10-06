a388bc7a2500a8c30ac5df823db1892a989c9c14
Mubarek Seid <mubareksd@gmail.com>
2022-10-06 16:35:42 +0300

    task 17

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -59,7 +59,7 @@ class Base:
         Args:
             json_string: is a string representing a list of dictionaries
         """
-        if json_string is None or json_string == []:
+        if json_string is None or len(json_string) == 0:
             return "[]"
         else:
             return json.loads(json_string)
