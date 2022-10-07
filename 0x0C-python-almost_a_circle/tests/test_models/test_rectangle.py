217ff529bbc8b71334937f8e5a0e811b8205d581
Mubarek Seid <mubareksd@gmail.com>
2022-10-07 19:49:36 +0300

    test

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -18,11 +18,14 @@ class TestRectangle(unittest.TestCase):
         """
         test 1
         """
+        r0 = Rectangle(1, 2)
+        self.assertEqual(r0.id, 1)
+
         r1 = Rectangle(10, 2)
-        self.assertEqual(r1.id, 1)
+        self.assertEqual(r1.id, 2)
 
         r2 = Rectangle(2, 10)
-        self.assertEqual(r2.id, 2)
+        self.assertEqual(r2.id, 3)
 
         r3 = Rectangle(10, 2, 0, 0, 12)
         self.assertEqual(r3.id, 12)
