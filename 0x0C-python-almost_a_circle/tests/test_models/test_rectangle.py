62c323ac8853d1d606b4373eb402fbed8c374af1
Mubarek Seid <mubareksd@gmail.com>
2022-10-08 09:59:19 +0300

    test

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -5,6 +5,8 @@ test_rectangle module
 import unittest
 from models.base import Base
 from models.rectangle import Rectangle
+from io import StringIO
+import sys
 
 
 class TestRectangle(unittest.TestCase):
@@ -23,8 +25,41 @@ class TestRectangle(unittest.TestCase):
         self.assertEqual(r0.x, 0)
         self.assertEqual(r0.y, 0)
         self.assertEqual(r0.__str__(), "[Rectangle] (1) 0/0 - 1/2")
-        r0.width = 3
-        self.assertEqual(r0.width, 3)
+        r0.width = 2
+        self.assertEqual(r0.width, 2)
+        self.assertEqual(r0.area(), 4)
+        capturedOutput = StringIO()
+        sys.stdout = capturedOutput
+        r0.display()
+        sys.stdout = sys.__stdout__
+
+        output = '##\n##\n'
+        self.assertEqual(capturedOutput.getvalue(), output)
+        output = {'x': 0, 'y': 0, 'id': 1, 'height': 2, 'width': 2}
+        self.assertEqual(r0.to_dictionary(), output)
+        r0.update()
+        self.assertEqual(r0.__str__(), "[Rectangle] (1) 0/0 - 2/2")
+        r0.update(89)
+        self.assertEqual(r0.__str__(), "[Rectangle] (89) 0/0 - 2/2")
+        r0.update(89, 1)
+        self.assertEqual(r0.__str__(), "[Rectangle] (89) 0/0 - 1/2")
+        r0.update(89, 1, 2)
+        self.assertEqual(r0.__str__(), "[Rectangle] (89) 0/0 - 1/2")
+        r0.update(89, 1, 2, 3)
+        self.assertEqual(r0.__str__(), "[Rectangle] (89) 3/0 - 1/2")
+        r0.update(89, 1, 2, 3, 4)
+        self.assertEqual(r0.__str__(), "[Rectangle] (89) 3/4 - 1/2")
+        r0.update(**{ 'id': 89 })
+        self.assertEqual(r0.__str__(), "[Rectangle] (89) 3/4 - 1/2")
+        r0.update(**{ 'id': 89, 'width': 1 })
+        self.assertEqual(r0.__str__(), "[Rectangle] (89) 3/4 - 1/2")
+        r0.update(**{ 'id': 89, 'width': 1, 'height': 2 })
+        self.assertEqual(r0.__str__(), "[Rectangle] (89) 3/4 - 1/2")
+        r0.update(**{ 'id': 89, 'width': 1, 'height': 2, 'x': 3 })
+        self.assertEqual(r0.__str__(), "[Rectangle] (89) 3/4 - 1/2")
+        r0.update(**{ 'id': 89, 'width': 1, 'height': 2, 'x': 3, 'y': 4 })
+        self.assertEqual(r0.__str__(), "[Rectangle] (89) 3/4 - 1/2")
+
 
     def test_2(self):
         """test 2"""
@@ -97,11 +132,6 @@ class TestRectangle(unittest.TestCase):
         """test 12"""
         with self.assertRaisesRegex(ValueError, "height must be > 0"):
             Rectangle(1, 0)
-
-    def test_13(self):
-        """test 13"""
-        r0 = Rectangle(2, 3)
-        self.assertEqual(r0.area(), 6)
 
 
 if __name__ == '__main__':
