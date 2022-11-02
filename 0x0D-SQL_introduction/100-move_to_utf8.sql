05052b4fa0e3209e9c7c68490ea8d082e6bb6ec0
Mubarek Seid <mubareksd@gmail.com>
2022-11-02 06:09:28 +0300

    task 17

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -3,5 +3,3 @@
 ALTER DATABASE hbtn_0c_0 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
 -- converts table to utf8
 ALTER TABLE hbtn_0c_0.first_table CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
--- converts a field in a table to utf8
-ALTER TABLE hbtn_0c_0.first_table CHANGE `name` VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
