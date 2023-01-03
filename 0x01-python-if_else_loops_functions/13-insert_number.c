48a292ba717acd503b4c63b3306946b209a172ea
Shell-thon <shellterminal0@gmail.com>
2023-01-03 19:13:27 +0100

    update

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,55 @@
+#include "lists.h"
+
+
+/**
+ * insert_node - Inserts a number into a
+ * sorted singly linked list
+ * @head: Pointer to the head of the list
+ * @number: integer to insert
+ *
+ * Return: Address of the new node, or NULL
+ * if it failed
+ **/
+listint_t *insert_node(listint_t **head, int number)
+{
+  listint_t *cur;
+  listint_t *new;
+  listint_t *temp;
+
+  cur = *head;
+  if (cur == NULL)
+    {
+      cur = malloc(sizeof(*cur));
+      if (cur == NULL)
+	return (NULL);
+      cur->n = number;
+      cur->next = NULL;
+      *head = cur;
+      return (cur);
+    }
+  while (cur != NULL)
+    {
+      if (cur->n >= number)
+	{
+	  new = malloc(sizeof(*new));
+	  new->n = cur->n;
+	  cur->n = number;
+	  temp = cur->next;
+	  cur->next = new;
+	  new->next = temp;
+	  return (cur);
+	}
+      if (cur->next == NULL)
+	{
+	  new = malloc(sizeof(*new));
+	  if (new == NULL)
+	    return (NULL);
+	  new->n = number;
+	  cur->next = new;
+	  new->next = NULL;
+	  return (cur);
+	}
+      cur = cur->next;
+    }
+  return (NULL);
+}
---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def uppercase(str):
+    sen = ''
+    for c in str:
+	i = ord(c)
+	if i >= 97 and i <= 122:
+		sen += chr(i - 32)
+	else:
+		sen += chr(i)
+    print('{}'.format(sen))
