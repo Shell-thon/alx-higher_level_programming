6a867b174b72870684db9880382faeaaa7a960e2
Mubarek Seid <mubareksd@gmail.com>
2023-01-17 14:07:20 +0300

    task 3

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -5,6 +5,9 @@ const request = require('request');
 request('http://swapi.co/api/films/' + process.argv[2], function (error, response, body) {
   if (error) {
     console.error(error);
+  } else if (response.statusCode == 200) {
+    console.log(JSON.parse(body).title);
+  } else {
+    console.log('Error code: ' + response.statusCode);
   }
-  console.log(JSON.parse(body).title);
 });
