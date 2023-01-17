c3d8d5ee252433cfaa04e4b21a2d75de151d1dd9
Mubarek Seid <mubareksd@gmail.com>
2023-01-17 14:26:32 +0300

    task 8

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,33 +1,7 @@
 #!/usr/bin/node
 
 const request = require('request');
-
-/*
-let characters = [];
-let dict = {};
-
-function addToDict (url, name) {
-  dict[url] = name;
-}
-
-request('http://swapi.co/api/films/' + process.argv[2], function (error, response, body) {
-  if (error) {
-    console.error(error);
-  }
-  characters = JSON.parse(body).characters
-  characters.forEach(function (url) {
-    request(url, function (error, response, body) {
-      if (error) {
-        console.error(error);
-      }
-      addToDict(url, JSON.parse(body).name);
-    });
-  });
-  characters.forEach(function (item) {
-    console.log(dict[item]);
-  })
-});
-*/
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
 
 function helpRequest (arr, i) {
   if (i === arr.length) {
@@ -35,14 +9,14 @@ function helpRequest (arr, i) {
   }
   request(arr[i], function (error, response, body) {
     if (error) {
-      console.error(error);
+      console.log(error);
     }
     console.log(JSON.parse(body).name);
     helpRequest(arr, i + 1);
   });
 }
 
-request('http://swapi.co/api/films/' + process.argv[2], function (error, response, body) {
+request(url, function (error, response, body) {
   if (error) {
     console.error(error);
   }
