6e5c380978ed6803f283179205186de076ccaa7a
Shell-thon <shellterminal0@gmail.com>
2023-01-24 01:13:44 +0100

    update

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -18,7 +18,7 @@ def list_division(my_list_1, my_list_2, list_length):
         except ZeroDivisionError:
             new.append(0)
             print("division by 0")
-        except:
+        except Exception:
             pass
         finally:
             pass
