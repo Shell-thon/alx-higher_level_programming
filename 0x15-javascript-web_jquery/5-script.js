faf7fddd4973854afaa0fb49f89cde0e672b5530
Mubarek Seid <mubareksd@gmail.com>
2023-01-25 11:47:53 +0300

    task 5

--- 0x15-javascript-web_jquery/5-script.js
+++ 0x15-javascript-web_jquery/5-script.js
@@ -1,5 +1,3 @@
-$(function () {
-  $('DIV#add_item').click(function () {
-    $('UL.my_list').append('<li>Item</li>');
-  });
+$('DIV#add_item').click(function () {
+  $('UL.my_list').append('<li>Item</li>');
 });
