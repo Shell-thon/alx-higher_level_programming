0172de283e8a69c21583916fbdbc29bd7d460cc8
Shell <113628692+Shell-thon@users.noreply.github.com>
2023-06-16 09:33:25 +0100

    Update 5-request_store.js

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,14 +1,12 @@
 #!/usr/bin/node
-/* gets contents of a webpage and stores it in a file */
 
 const request = require('request');
 const fs = require('fs');
 
-const args = process.argv.slice(2);
-const url = args[0];
-const path = args[1];
-
-request
-  .get(url)
-  .on('error', err => { console.log(err); })
-  .pipe(fs.createWriteStream(path, 'utf-8'));
+request(process.argv[2], function (_err, _res, body) {
+  fs.writeFile(process.argv[3], body, 'utf8', function (err) {
+    if (err) {
+      console.log(err);
+    }
+  });
+});
