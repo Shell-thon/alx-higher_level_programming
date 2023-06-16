d71b362cffdd6bbe4c993fec6e93f53fbdd94835
Shell <113628692+Shell-thon@users.noreply.github.com>
2023-06-16 09:29:15 +0100

    Update 2-statuscode.js

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,10 +1,7 @@
 #!/usr/bin/node
-/* displays the status code of a GET request */
 
 const request = require('request');
 
-const url = process.argv.slice(2)[0];
-
-request(url, (err, res, body) => {
-  (err) ? console.log(err) : console.log(`code: ${res.statusCode}`);
+request(process.argv[2], function (_err, res) {
+  console.log('code:', res.statusCode); // Print the response status code if a response was received
 });
