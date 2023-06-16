bb8b7f4e3562e1b05ddd073cdb8d2bdf27b8982d
Shell <113628692+Shell-thon@users.noreply.github.com>
2023-06-16 09:29:58 +0100

    Update 3-starwars_title.js

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,17 +1,9 @@
 #!/usr/bin/node
-/*
-    prints the title of a star wars movie
-    given integer supposed to match episode number
-
-*/
 
 const request = require('request');
+const starWarsUri = 'https://swapi-api.hbtn.io/api/films/'.concat(process.argv[2]);
 
-const num = process.argv.slice(2)[0];
-const url = `https://swapi-api.hbtn.io/api/films/${num}`;
-
-request(url, (err, res, body) => {
-  if (err) console.log(err);
-  const output = JSON.parse(body).title;
-  console.log(output);
+request(starWarsUri, function (_err, _res, body) {
+  body = JSON.parse(body);
+  console.log(body.title);
 });
